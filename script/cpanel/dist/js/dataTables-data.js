4342512d6f26ceb4c8f7dd5f14fb0fd808edded1
Ayman Shaikh <67065247+ayman-sys@users.noreply.github.com>
2022-10-12 23:24:04 +0530

    Add files via upload

---
+++ script/cpanel/dist/js/dataTables-data.js
@@ -0,0 +1,11 @@
+
+"use strict";
+
+$(document).ready(function() {
+	"use strict";
+
+	$('#datable_1').DataTable({
+    "ordering": false
+});
+    $('#datable_2').DataTable({ "lengthChange": false});
+} );
